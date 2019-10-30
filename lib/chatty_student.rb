90278cd341794ca4cbd81272dad114fa8fb58079
Jazmin M <jazminmerckel@gmail.com>
2019-10-30 04:07:14 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,6 +7,15 @@ class ChattyStudent < Student
   
   def raise_hand
     super
-    
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    puts "Pick me!"
   end
 end
